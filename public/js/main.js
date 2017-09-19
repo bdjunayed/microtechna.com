236d33b866866b64aafdaec38906b5feceb4f4e4
Engr. Junayed Chowdhury <bdjunayed@gmail.com>
2017-09-19 16:02:43 +0600

    form run dev

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,168 @@
-!function(e){function o(i){if(n[i])return n[i].exports;var t=n[i]={i:i,l:!1,exports:{}};return e[i].call(t.exports,t,t.exports,o),t.l=!0,t.exports}var n={};o.m=e,o.c=n,o.d=function(e,n,i){o.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:i})},o.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(n,"a",n),n},o.o=function(e,o){return Object.prototype.hasOwnProperty.call(e,o)},o.p="",o(o.s=35)}({35:function(e,o,n){e.exports=n(36)},36:function(e,o){$(document).ready(function(){$(".DesignSlideTwo").owlCarousel({items:1,loop:!1,nav:!1,dots:!1,pagination:!0,touchDrag:!0,responsiveClass:!0,navigationText:['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],navigation:!0,autoPlay:!1,mouseDrag:!0,responsive:{0:{items:1},640:{items:3},1e3:{items:6}}}),$(".DSliderThree").owlCarousel({items:1,loop:!0,nav:!1,dots:!1,pagination:!0,touchDrag:!1,responsiveClass:!0,navigationText:['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],navigation:!0,autoPlay:!1,mouseDrag:!0,responsive:{0:{items:1},640:{items:1},1e3:{items:1}}}),$(window).on("scroll",function(){$(window).scrollTop()>0?$(".Mfix").addClass("fixedHeader"):$(".Mfix").removeClass("fixedHeader")}),$(".MobileMenu").on("click",function(){$(".Menu > ul").slideToggle("slow")}),$(".SubSer").on("click",function(){$(".Menu ul li .Submenu").slideToggle("slow")}),$(window).on("scroll",function(){$(window).scrollTop()>1660?$(".BackTo").fadeIn("slow"):$(".BackTo").fadeOut("slow")}),$("body, html").on("click",".BackTo",function(e){e.preventDefault(),$("html, body").animate({scrollTop:0},800)})})}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 37);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 37:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(38);
+
+
+/***/ }),
+
+/***/ 38:
+/***/ (function(module, exports) {
+
+
+$(document).ready(function () {
+    /*Design Page Slidertwo**/
+    $('.DesignSlideTwo').owlCarousel({
+        items: 1,
+        loop: false,
+        nav: false,
+        dots: false,
+        pagination: true,
+        touchDrag: true,
+        responsiveClass: true,
+        navigationText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+        navigation: true,
+        autoPlay: false,
+        mouseDrag: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+    /*DesignPage Slider Three Start here*/
+    $('.DSliderThree').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: false,
+        pagination: true,
+        touchDrag: false,
+        responsiveClass: true,
+        navigationText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+        navigation: true,
+        autoPlay: false,
+        mouseDrag: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            640: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    /*DesignPage Slider Three End here*/
+    /*FixHeader Start By Shagor */
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 0) {
+            $(".Mfix").addClass('fixedHeader');
+        } else {
+            $(".Mfix").removeClass('fixedHeader');
+        }
+    });
+
+    /*FixHeader End By Shagor */
+
+    $(".MobileMenu").on('click', function () {
+        $(".Menu > ul").slideToggle('slow');
+    });
+    $(".SubSer").on('click', function () {
+        $(".Menu ul li .Submenu").slideToggle('slow');
+    });
+
+    /*MobileMenu End By Shagor */
+
+    /*back to top by shagor*/
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 1660) {
+            $(".BackTo").fadeIn('slow');
+        } else {
+            $(".BackTo").fadeOut('slow');
+        }
+    });
+    $("body, html").on("click", ".BackTo", function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 800);
+    });
+});
+
+/***/ })
+
+/******/ });
