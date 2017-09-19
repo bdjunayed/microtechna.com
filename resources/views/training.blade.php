9f3d0ab903ee12cb1d139a32a04fae3e0d075d89
Hasan Farid <faridhasan@gmail.com>
2017-09-19 14:51:04 +0600

    Finally Update content

--- resources/views/training.blade.php
+++ resources/views/training.blade.php
@@ -39,9 +39,8 @@
                                 <br> কিন্তু আমি পণ করেছিলাম
                                 <br>আর নিজের মনকে বলেছিলাম
                                 <br>এখন কষ্ট কর যাতে বাকি সারা জীবন
-                                <br>চ্যাম্পিয়ন হয়ে বেচে থাক
-                            <i class="fa fa-quote-right" aria-hidden="true"></i><br>
-                                <span>- Muhammmed Ali</span> </p>
+                                <br>চ্যাম্পিয়ন হয়ে বেঁচে থাক <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                <br> <span>- Muhammmed Ali</span> </p>
                         </div>
                     </div>
                 </div>
@@ -65,7 +64,6 @@
                 <div class="experienceMic">
                     <h2>অভিজ্ঞ আমেরিকান আইটি প্রফেশনাল দ্বারা পরিচালিত বাংলাদেশে এই প্রথম ও একমাত্র ব্যতিক্রমধর্মী ট্রেনিং সেন্টার মাইক্রোটেক এ আপনি পাবেন:</h2>
                     <hr>
-          
                     <div class="bollet">
                         <li><b>প্রতিদিন ১২(বার) ঘন্টা করে ক্লাস  ও প্রাকটিস সেশন</b>
                             <p>এর উদ্দেশ্য হচ্ছে প্রতিদিনের ক্লাসের সিলেবাস হাতে-কলমে ফূলটাইম টিচারদের দ্বারা বার বার অনুশীলনের মাধ্যমে মুখস্ত করানো</p>
@@ -90,8 +88,10 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="experienceMic messanger-text">
-            <p>আপনার পরিচিত কেও কি Familly Migration(I-130) এ আমিরিকাতে আসবে ? আমিরিকাতে আসার আগে বাংলাদেশ থেকে উপযুক্ত ট্রেনিং নিয়ে সরাসরি আইটি জব শুরু করতে আমরা সাহায্য করে থাকি ।<br>
-                    বাংলাদেশ থেকে আমেরিকায় Familly migration or Other Visa নিয়ে আসার আগে তিন/ছয় মাসের কোর্স করে আসুন কারন এই কোর্স আমেরিকাতে করতে কয়েকগুন খরচ করেও এই ধরনের সুবিধা পাবেন না । মোটকথা আপনার সময় বের করাটা অসম্ভব হবে এবং আমেরিকার কোন ট্রেনিং সেন্টার আপনাকে এভাবে সার্ভিস দিবে না । আমাদের ২০ বছরের অবিজ্ঞতার আলোকে সাজানো এই প্রথম ও একমাত্র ব্যতিক্রমধর্মী ট্রেনিং সেন্টার আপনাদের সেবায় সদা প্রস্তুত । আমেরিকাতে যারা বসবাস করছেন তারাও তিন মাসের জন্য বাংলাদেশে ভিজিট করে আমাদের এই সেবা গ্রহন করতে পারেন ।<br><br> যদি তাও সম্ভব না হয়, তবে আমাদের One-to-One কোর্স এ অল্প খরচ এ ক্লাস করতে পারেন । One-to-One কোর্স এ আপনাকে একজন দক্ষ আইটি প্রফেশনাল দ্বারা হাতে কলমে বুঝানো হবে, বিস্তারিত জানতে কল করুন </p>
+                    <p>আপনার পরিচিত কেও কি Familly Migration(I-130) এ আমিরিকাতে আসবে ? আমিরিকাতে আসার আগে বাংলাদেশ থেকে উপযুক্ত ট্রেনিং নিয়ে সরাসরি আইটি জব শুরু করতে আমরা সাহায্য করে থাকি ।
+                        <br> বাংলাদেশ থেকে আমেরিকায় Familly migration or Other Visa নিয়ে আসার আগে তিন/ছয় মাসের কোর্স করে আসুন কারন এই কোর্স আমেরিকাতে করতে কয়েকগুন খরচ করেও এই ধরনের সুবিধা পাবেন না । মোটকথা আপনার সময় বের করাটা অসম্ভব হবে এবং আমেরিকার কোন ট্রেনিং সেন্টার আপনাকে এভাবে সার্ভিস দিবে না । আমাদের ২০ বছরের অবিজ্ঞতার আলোকে সাজানো এই প্রথম ও একমাত্র ব্যতিক্রমধর্মী ট্রেনিং সেন্টার আপনাদের সেবায় সদা প্রস্তুত । আমেরিকাতে যারা বসবাস করছেন তারাও তিন মাসের জন্য বাংলাদেশে ভিজিট করে আমাদের এই সেবা গ্রহন করতে পারেন ।
+                        <br>
+                        <br> যদি তাও সম্ভব না হয়, তবে আমাদের One-to-One কোর্স এ অল্প খরচ এ ক্লাস করতে পারেন । One-to-One কোর্স এ আপনাকে একজন দক্ষ আইটি প্রফেশনাল দ্বারা হাতে কলমে বুঝানো হবে, বিস্তারিত জানতে কল করুন </p>
                 </div>
             </div>
         </div>
@@ -101,9 +101,9 @@
             <div class="col-md-6">
                 <div class="experienceMic custom">
                     <div class="single-ms"> <img src="./images/borkot-face.jpg" alt="Software Testing" />
-                        <p>Our Goal is NOT guide you one or two days in a week for few hours then tell you to go home and study hard by youself alone even though we know more then 80% student don't folllow . Obviously our is to provide you IT carer knowledge confirmly in to your brain through ther best way to get and maintain IT job for the rest of your life! </p>
+                        <p>Our goal is not to guide you one or two days for a few hours in a week then tell you to go home and study hard by yourself alone even though we know more than 80% student don't follow. Obviously, our goal is to provide you IT Career Knowledge confidently in to your brain through the best way to get and maintain an IT job for the rest of your life! </p>
                         <h4><b>-Barakat Chowdhury</b></h4>
-                        <h4>Sofware Engineer, Washington DC </h4>
+                        <h4>Software Engineer, Washington DC </h4>
                         <p> BSc in Computer Science and Engg, MS, USA
                             <br> Oracle Certified Professional</p>
                     </div>
@@ -112,9 +112,9 @@
             <div class="col-md-6">
                 <div class="experienceMic custom">
                     <div class="single-ms"> <img src="./images/belal-face.jpg" alt="Software Testing" />
-                        <p>We have over 20 years of teaching experience in IT industry. We have specificly know student's behaviour culture, motive, weak points, and techniques to make every student success in life. Therefore, we have no otherchoices except providing the best career solution to community.</p>
-                        <h4><b>-Balal Chowdhury</b></h4>
-                        <h4>Sofware Architect, NY</h4>
+                        <p>We have over 20 years of teaching experience in IT industry. We clearly know student's behavior, culture, motive, weak points, and techniques to make every student success in life. Therefore, we have no other choices except providing the best career solution to the community.</p>
+                        <h4><b>-Belal Chowdhury</b></h4>
+                        <h4>Software Architect, NY</h4>
                         <p>BSc in Computer Science, MS, USA
                             <br> Microsoft Certified Solutions Expert
                             <br> Oracle Certified Professional</p>
@@ -134,8 +134,7 @@
         <hr>
         <div class="row">
             <div class="col-sm-6 col-md-4">
-                <div class="single-course"> 
-                   <img src="./images/testing-01.jpg"  alt="Software Testing" />
+                <div class="single-course"> <img src="./images/testing-01.jpg" alt="Software Testing" />
                     <div class="course-details">
                         <h4><b>Software Testing</b></h4>
                         <p>This course will prepare you to get the position of an exceptional Software Test Engineer (Selenium/Java or QTP/UFT VBScript) platform to work with any cross-functional team, and join in the world-class community of talented experts.</p>
@@ -145,8 +144,7 @@
                 </div>
             </div>
             <div class="col-sm-6 col-md-4">
-                <div class="single-course"> 
-                   <img src="./images/sqlserverDBA.jpg" alt="Software Testing" />
+                <div class="single-course"> <img src="./images/sqlserverDBA.jpg" alt="Software Testing" />
                     <div class="course-details">
                         <h4><b>SQL Server DBA</b></h4>
                         <p>This will make you ready to take responsible for the evaluation, installation, database backup and recovery, data integrity procedures, space management strategies, backup strategy and the testing of disaster recovery processes, database security, maintenance, and support for enterprise class database management systems.</p>
@@ -155,20 +153,8 @@
                     </div>
                 </div>
             </div>
-          <div class="col-sm-6 col-md-4">
-                <div class="single-course"> 
-                   <img src="./images/web-01.jpg"  alt="Software Testing" />
-                    <div class="course-details">
-                        <h4><b>Web App Developer</b></h4>
-                        <p>This trainee will be able to produce clean, semantic, object-oriented code to build excellent Web Application with the ability to adopt existing coding styles as appropriate in a range of languages such as PHP/ JavaScript using Laravel framework. Also will have the ability to develop both front and back-end systems, troubleshoot, test and maintain the core product software, databases to ensure optimisation and functionality, design and document code, identify scope, key requirements, specification, creative approach and development methodology for projects.</p>
-                        <hr>
-                        <p><span class="text-center">Duration: 3 or 6 months</span></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="single-course">
-                    <img src="./images/cisco.jpg" alt="Software Testing" />
+            <div class="col-sm-12 col-md-4">
+                <div class="single-course"> <img src="./images/ccna.png" alt="Software Testing" />
                     <div class="course-details">
                         <h4><b>Cisco Certified Network Associate - CCNA</b></h4>
                         <p>This course will furnish you as a Network Operations Engineer with the Cisco Certified Network Associate (CCNA) certificate having proper knowledge on network implementations, routing and switching (10/100/1000 Ethernet, Frame Relay, Bridging, TCP/IP, IPX, EIGRP, and trunking protocols) in operations and implementations, firewall, VPN, security skills.</p>
@@ -177,10 +163,10 @@
                     </div>
                 </div>
             </div>
-     
-
-            <div class="col-sm-12 col-md-4">
-                <div class="single-course"> <img src="./images/testing.jpg"  alt="Software Testing" />
+            <div class="spacer-60"></div>
+            <div class="clearfix"></div>
+            <div class="col-sm-6 col-md-4">
+                <div class="single-course"> <img src="./images/linux.jpg" alt="Software Testing" />
                     <div class="course-details">
                         <h4><b>Red Hat Certified Engineer - RHCE</b></h4>
                         <p>To get a job as a System Administrator having proper understanding and experience on installing and maintaining servers as well as ensuring clients get a good service, helping to engineer high availability in solutions, through applying specific tuning, load testing, webservers, database servers, high availability, and high performance.</p>
@@ -189,9 +175,18 @@
                     </div>
                 </div>
             </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="single-course"> <img src="./images/web-01.jpg" alt="Software Testing" />
+                    <div class="course-details">
+                        <h4><b>Web App Developer</b></h4>
+                        <p>This trainee will be able to produce clean, semantic, object-oriented code to build excellent Web Application with the ability to adopt existing coding styles as appropriate in a range of languages such as PHP/ JavaScript using Laravel framework. Also will have the ability to develop both front and back-end systems, troubleshoot, test and maintain the core product software, databases to ensure optimisation and functionality, design and document code, identify scope, key requirements, specification, creative approach and development methodology for projects.</p>
+                        <hr>
+                        <p><span class="text-center">Duration: 3 or 6 months</span></p>
+                    </div>
+                </div>
+            </div>
             <div class="col-sm-12 col-md-4">
-                <div class="single-course">  
-                   <img src="./images/mobile.jpg"  alt="Software Testing" />
+                <div class="single-course"> <img src="./images/mobile.jpg" alt="Software Testing" />
                     <div class="course-details">
                         <h4><b>Mobile App Developer</b></h4>
                         <p>We will furnish you with proven ability of working experience building apps in Java and Aroid SDK using JSON and REST services in an agile team following scrum methodology, proficiency of using Gradle/ Jenkins/ Git/ Jira/ Confluence, JSON and XML, multi-site/ multi-lingual environments, building, packaging and deploying code for continuous integration, supporting and troubleshooting code issues in test and live environments.</p>
@@ -200,50 +195,53 @@
                     </div>
                 </div>
             </div>
-
         </div>
-        <div class="spacer-60"></div>
-        <div class="contact-us">
-     
-            <div class="row">
-
-                <h2 class="text-center">বিস্তারিত জানতে কল করুন আমাদের নিউ ইউর্ক অথবা ঢাকা সেন্টার এ </h2>
-                <div class="col-sm-4 col-md-4">
-                    <div class="single-content">
-                        <h3>USA Corporate Office </h3>
-                        <p>167-17-Hillside Ave, 2FL</p>
-                        <p>Jamaica, NY 11432</p>
-                        <p><b>Call Us:</b></p>
-                        <p> 929.200.0604</p>
-                        <p>917.257.4204</p>
-                        <p>917.826.6741</p>
-                    </div>
+        <div class="spacer-30"></div>
+    </div>
+</div>
+<!--.Contact-us-area-->
+<div class="contact-us experienceMic">
+    <div class="container">
+        <div class="row">
+            <h2 class="text-center">বিস্তারিত জানতে কল করুন আমাদের নিউ ইউর্ক অথবা ঢাকা সেন্টার এ </h2>
+            <div class="col-sm-4 col-md-4">
+                <div class="single-content">
+                    <h3>USA Corporate Office </h3>
+                    <p>167-17, Hillside Ave, 2FL</p>
+                    <p>Jamaica, NY 11432</p>
+                    <p><b>Call, Text, Meassage:</b></p>
+                    <p> 929.200.0604</p>
+                    <p>917.257.4204</p>
+                    <p>917.826.6741</p>
                 </div>
-                <div class="col-sm-4 col-md-4">
-                    <div class="single-content custom-center">
-                        <div class="logo"> <a href="{{ url('/') }}">MicroTech NA</a> </div>
-                        <p>Call for clarifiaction</p>
-                        <h3>1.888.612.7778</h3> </div>
-                </div>
-                <div class="col-sm-4 col-md-4">
-                    <div class="single-content text-right">
-                        <h3>Dhaka Branch Office </h3>
-                         <div class="custom-right">
-                              <p>kha 21, Road 5, Nikunjo-2</p>
-                        <p>Call: 017.2513.9901</p>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="single-content custom-center">
+                    <div class="logo"> <a href="{{ url('/') }}">MicroTech NA, Inc.</a> </div>
+                    <p>Call for clarifiaction</p>
+                    <h3>1.888.612.7778</h3> </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="single-content text-right">
+                    <h3>Dhaka Branch Office </h3>
+                    <div class="custom-right">
+                        <p>Kha 21, Road 5, Nikunjo 2</p>
+                        <p>Dhaka 1215, Bangladesh</p>
+                        <p><b>Call, Text, Meassage:</b></p>
+                        <p>017.2513.9901</p>
                         <p>018.1751.2688</p>
                         <p>017.1056.3784</p>
-                         </div>
                     </div>
                 </div>
             </div>
-            <div class="spacer-30"></div>
-        <div class="row">
-               <div class="col-md-12">
-                   <script type="text/javascript" src="https://form.jotform.me/jsform/72484192474463"></script>
-               </div>
-           </div>
         </div>
-        <div class="spacer-60"></div>
+        <div class="spacer-30"></div>
+        <div class="row">
+            <div class="col-md-12">
+                <script type="text/javascript" src="https://form.jotform.me/jsform/72484192474463"></script>
+            </div>
+        </div>
     </div>
-</div> @endsection
+    <div class="spacer-60"></div>
+</div>
+<!--.Contact-us-area-->@endsection
