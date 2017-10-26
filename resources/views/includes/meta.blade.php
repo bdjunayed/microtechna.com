d6100fe52d35e1e6e9595a6d97d44f00d8d3ba51
Engr. Junayed Chowdhury <bdjunayed@gmail.com>
2017-10-27 00:21:14 +0600

    fine tuning before production

--- resources/views/includes/meta.blade.php
+++ resources/views/includes/meta.blade.php
@@ -19,28 +19,3 @@
     @if (getenv('BING-WEBMASTER'))
         <meta name="msvalidate.01" content="{{ getenv('BING-WEBMASTER') }}" />
     @endif
-
-<!--     {% if page.excerpt %}
-    <meta name="description" content="{{ page.excerpt| strip_html }}" />
-    <meta property="og:description" content="{{ page.excerpt| strip_html }}" />
-    {% else %}
-    <meta name="description" content="{{ site.description }}">
-    <meta property="og:description" content="{{ site.description }}" />
-
-    <meta name="author" content="{{ site.name }}" />
-
-    {% if page.title %}
-    <meta property="og:title" content="{{ page.title }}" />
-    <meta property="twitter:title" content="{{ page.title }}" /> -->
-
-
-    <!-- Facebook and Twitter integration -->
-    <!-- <meta property="og:title" content=""/> -->
-<!--     <meta property="og:image" content=""/>
-    <meta property="og:url" content=""/>
-    <meta property="og:site_name" content=""/> -->
-    <!-- <meta property="og:description" content=""/> -->
-    <!-- <meta name="twitter:title" content="" /> -->
-<!--     <meta name="twitter:image" content="" />
-    <meta name="twitter:url" content="" />
-    <meta name="twitter:card" content="" /> -->
