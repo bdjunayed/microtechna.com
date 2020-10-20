1ff291771166ec4332cda3e1210ac5a7e9500f54
Junayed M Chowdhury <bdjunayed@gmail.com>
2020-10-20 19:21:10 +0600

    Campaigns Oct 2020

--- public/js/app.js
+++ public/js/app.js
@@ -27004,8 +27004,8 @@ var Form = /*#__PURE__*/function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\www\microtechna.com\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\www\microtechna.com\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\www\MicroTechNA.com\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\www\MicroTechNA.com\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
